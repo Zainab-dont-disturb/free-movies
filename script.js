f827989b8f26a5a75a335e8b39e12b5b791ad317
Zainab-dont-disturb <zainabadaranijo111@gmail.com>
2024-12-09 18:36:57 +0000

    done
    done

---
+++ script.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value){
+
+document.getElementById('display').value+=value;
+
+}
+
+function clearDisplay(){
+
+document.getElementById("display").value="";
+
+}
+
+function calculateResult(){
+
+let result=eval(document.getElementById('display').value);
+
+document.getElementById('display').value=result;
+
+
+}
